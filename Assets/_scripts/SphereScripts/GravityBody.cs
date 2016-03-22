a43e4ae43d1eb606a8ce240b26279ebbc44c0922
David Carlson <britishorbust@gmail.com>
2016-03-21 22:19:31 -0400

    Organized folders, imported scripts

---
+++ Assets/_scripts/SphereScripts/GravityBody.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour {
+
+    public GravityAttractor attractor;
+    private Transform MyTransform;
+    void Start()
+    {
+        Rigidbody myRigidbody = GetComponent<Rigidbody>();
+
+        myRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        myRigidbody.useGravity = false;
+        MyTransform = transform;
+    }
+
+    void Update()
+    {
+        attractor.Attract(MyTransform);
+    }
+}
